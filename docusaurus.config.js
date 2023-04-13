4dade86f414dbc0e8e6254dd1e7ba77f9bdba666
Howard Zhang <75195784+Howardzhangdqs@users.noreply.github.com>
2023-04-13 20:20:16 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,9 +53,8 @@ async function createConfig() {
 				'@docusaurus/plugin-ideal-image',
 				{
 					quality: 70,
-					max: 1030, // max resized image's size.
+					max: 1024, // max resized image's size.
 					min: 640, // min resized image's size. if original is lower, use that size.
-					steps: 2, // the max number of images generated between min and max (inclusive)
 					disableInDev: false,
 				},
 			],
